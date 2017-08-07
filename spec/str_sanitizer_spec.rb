850836df3830467c4b7297e4cc3c355cc1dc0711
Jakaria Blaine <jakariablaine120@gmail.com>
2017-08-07 19:07:29 +0600

    Add tests for single_quote method

--- spec/str_sanitizer_spec.rb
+++ spec/str_sanitizer_spec.rb
@@ -9,10 +9,22 @@ RSpec.describe StrSanitizer do
     expect(StrSanitizer.respond_to? :double_quote).to eq true
   end
 
+  it "has a method named 'single_quote'" do
+    expect(StrSanitizer.respond_to? :single_quote).to eq true
+  end
+
   it "return a sanitized string with escaped double-quote" do
     test_string = 'He said, "Look!"'
     sanitized_string = StrSanitizer.double_quote(test_string) 
 
     expect(sanitized_string).to eq('He said, \"Look!\"')
+  end
+
+
+  it "returns a sanitized string with escaped single-quote" do
+    test_string = "He said, 'Look!'"
+    sanitized_string = StrSanitizer.single_quote(test_string)
+
+    expect(sanitized_string).to eq("He said, \'Look!\'")
   end
 end
