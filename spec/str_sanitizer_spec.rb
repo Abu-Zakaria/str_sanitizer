aa7e4c401c00acc19e87fe16e9e07d8d4a826f50
Jakaria Blaine <jakariablaine120@gmail.com>
2017-08-08 18:41:07 +0600

    Remove specs for Quotes module in StrSanitizer class, respond_to? method will suffice

--- spec/str_sanitizer_spec.rb
+++ spec/str_sanitizer_spec.rb
@@ -5,30 +5,13 @@ RSpec.describe StrSanitizer do
     expect(StrSanitizer::VERSION).not_to be nil
   end
 
-  it "has a method named 'double_quote'" do
+  it "can invoke the methods of Quotes module" do
     expect(StrSanitizer.respond_to? :double_quote).to eq true
-  end
-
-  it "has a method named 'single_quote'" do
     expect(StrSanitizer.respond_to? :single_quote).to eq true
-  end
-
-  it "has a method named 'both_quotes'" do
     expect(StrSanitizer.respond_to? :both_quotes).to eq true
-  end
-
-  it "returns a sanitized string with escaped double-quote" do
-    test_string = 'He said, "Look!"'
-    sanitized_string = StrSanitizer.double_quote(test_string) 
-
-    expect(sanitized_string).to eq('He said, \\"Look!\\"')
-  end
-
-
-  it "returns a sanitized string with escaped single-quote" do
-    test_string = "He said, 'Look!'"
-    sanitized_string = StrSanitizer.single_quote(test_string)
-
-    expect(sanitized_string).to eq("He said, \\'Look!\\'")
+    expect(StrSanitizer.respond_to? :has_single_quote?).to eq true
+    expect(StrSanitizer.respond_to? :has_double_quote?).to eq true
+    expect(StrSanitizer.respond_to? :has_both_quotes?).to eq true
+    expect(StrSanitizer.respond_to? :has_any_quote?).to eq true
   end
 end
