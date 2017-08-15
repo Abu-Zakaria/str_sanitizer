8bafae11ed5ba5189c135caa0052b7204e880271
JakariaBlaine <jakariablaine120@gmail.com>
2017-08-15 15:42:07 +0600

    Update the method names according to the previous commit to the HtmlEntities  module

--- spec/str_sanitizer/html_entities_spec.rb
+++ spec/str_sanitizer/html_entities_spec.rb
@@ -10,18 +10,18 @@ RSpec.describe StrSanitizer::HtmlEntities do
   end
 
   it "has some method for encoding and decoding" do
-    expect(@methods.respond_to? :encode).to eq(true)
-    expect(@methods.respond_to? :decode).to eq(true)
+    expect(@methods.respond_to? :html_encode).to eq(true)
+    expect(@methods.respond_to? :html_decode).to eq(true)
   end
 
   it "returns a encoded string" do
-    encoded = @methods.encode(@encode_string)
+    encoded = @methods.html_encode(@encode_string)
     # The encoded string was done by `htmlentities` gem manually and pasted in here
     expect(encoded).to eq("&lt;div&gt;Hello world&lt;/div&gt;")
   end
 
   it "returns a decoded string" do
-    decoded = @methods.decode(@decode_string)
+    decoded = @methods.html_decode(@decode_string)
     # The decoded string was done by `htmlentities` gem manually and pasted in here
     expect(decoded).to eq("<div>Hello world</div>")
   end
@@ -29,8 +29,8 @@ RSpec.describe StrSanitizer::HtmlEntities do
   it "doesn't throw any exception if nothing was there to encode and decode" do
     simple_string = "Hello world"
 
-    expect(@methods.encode simple_string).to eq(simple_string)
-    expect(@methods.decode simple_string).to eq(simple_string)
+    expect(@methods.html_encode simple_string).to eq(simple_string)
+    expect(@methods.html_decode simple_string).to eq(simple_string)
   end
 end
 
