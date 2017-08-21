dbbad4edf879ff11f51b683c6b97170d9dda3085
JakariaBlaine <jakariablaine120@gmail.com>
2017-08-21 21:24:08 +0600

    Add tests for the 'html_encode' method with 'options' arguments

--- spec/str_sanitizer/html_entities_spec.rb
+++ spec/str_sanitizer/html_entities_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe StrSanitizer::HtmlEntities do
     encoded = @module.html_encode(@encode_string)
     # The encoded string was done by `htmlentities` gem manually and pasted in here
     expect(encoded).to eq("&lt;div&gt;Hello world&lt;/div&gt;")
+
+    # With option
+
+    encoded = @module.html_encode(@encode_string, :decimal)
+    expect(encoded).to eq("&#60;div&#62;Hello world&#60;/div&#62;")
+
+    encoded = @module.html_encode(@encode_string, :hexadecimal)
+    expect(encoded).to eq("&#x3c;div&#x3e;Hello world&#x3c;/div&#x3e;")
   end
 
   it "returns a decoded string" do
